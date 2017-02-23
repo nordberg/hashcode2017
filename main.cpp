b80401f4b6d9d0a63f83723c573199a0992c2d80
axelri <axel.riese@gmail.com>
2017-02-23 19:35:16 +0100

    parse more

--- main.cpp
+++ main.cpp
@@ -1,52 +1,54 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 
-struct Cache {
-    int id, latency;
-};
+#define DEBUG
 
 struct EndPoint {
-    int center_latency;
-    std::vector<Cache> caches;
+    EndPoint(int latency) : latency(latency) {}
+    int latency;
+    std::unordered_map<int,int> caches;
 };
 
 struct Request {
     int id, endpoint, num_req;
 };
 
-int main(int argc, char const *argv[]) {
-    int V, E, R, C, X;
+int V, E, R, C, X;
+std::vector<int> video_sizes;
+std::vector<EndPoint> end_points;
+std::vector<Request> requests;
+
+void read_input() {
     std::cin >> V >> E >> R >> C >> X;
 
-    std::vector<int> video_sizes;
     for (size_t i = 0; i < V; i++) {
         int s_i;
         std::cin >> s_i;
         video_sizes.emplace_back(s_i);
     }
 
-    std::vector<EndPoint> end_points;
     for (size_t i = 0; i < E; i++) {
         int latency;
         std::cin >> latency;
         int caches;
         std::cin >> caches;
-        std::vector<Cache> cache_v;
+        end_points.emplace_back(latency);
         for (size_t j = 0; j < caches; j++) {
             int id, cache_latency;
             std::cin >> id >> cache_latency;
-            cache_v.emplace_back(Cache{id, cache_latency});
+            end_points.back().caches[id] = cache_latency;
         }
-        end_points.emplace_back(EndPoint{latency, cache_v});
     }
 
-    std::vector<Request> requests;
     for (size_t i = 0; i < R; i++) {
         int id, endpoint, num_req;
         std::cin >> id >> endpoint >> num_req;
         requests.emplace_back(Request{id, endpoint, num_req});
-    }
+    }}
 
+int main(int argc, char const *argv[]) {
+    read_input();
     return 0;
 }
